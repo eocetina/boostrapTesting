f1174babfbe54353e41f815c074a53d032054976
Ed Cetina <eocetina@gmail.com>
2023-08-04 11:59:46 -0600

    ci:nuevos funcionalidades y tarea

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+console.log("Página en construcción con Patricio Estrella");
+
+function clickPatricio() {
+    alert("La mayonesa es un instrumento?");
+}
+
+function changeColor( elementHtml, color ) {
+    // alert("Que emoción, voy a cambiar mi color")
+    console.log(elementHtml);
+    console.log(color);
+    
+    elementHtml.style.color = color;
+}
