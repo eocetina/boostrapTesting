933683fad2cef2dcedcee05aa69eaf66a687848f
Ed Cetina <eocetina@gmail.com>
2023-08-04 12:07:42 -0600

    ci:new mods

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,33 @@ function changeColor( elementHtml, color ) {
     
     elementHtml.style.color = color;
 }
+
+function changeColorWithPrompt( elementHtml) {
+    const color = prompt("Dime el color en inglés", "yellow");
+    changeTextOfUserColor( elementHtml, color);
+    changeColor( elementHtml, color );
+}
+
+function changeTextOfUserColor( element, color ) {
+    element.innerHTML = "<h3>Ahora soy <em>" + color + "</em> </h3>";
+}
+
+function resetColor(){
+    const refRedColor = document.getElementById("red-color");
+    const refGreenColor = document.getElementById("green-color");
+    const refPurpleColor = document.getElementById("purple-color");
+    const refUserColor = document.getElementById("user-color");
+
+    // refRedColor.style.color = "black";
+    changeColor(refRedColor, 'black');
+    changeColor(refGreenColor, 'black');
+    changeColor(refPurpleColor, 'black');
+    changeColor(refUserColor, 'black');
+    changeTextOfUserColor( refUserColor, 'black');
+}
+
+function changeName(){
+    const userName = prompt("Escribe tu nombre cholo", "Machete");
+    const refGretting = document.getElementById("gretting");
+    refGretting.innerHTML = `Hola ${userName}, la clika te respalda`;
+}
